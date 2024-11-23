40e4f11ff6f643725ccc8511cf4444b551b45f7a
Kostiantyn Kliukhin <kostiantyn.kliukhin@codeandcakes.com>
2024-11-23 16:11:53 +0200

    new domain

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ async function bootstrap() {
       'http://localhost:3000',
       'https://vtnews.ai',
       'http://localhost.charlesproxy.com:3000',
+      'https://www.my-test-domain.xyz',
+      'https://my-test-domain.xyz',
     ],
     methods: ['GET', 'POST'],
     credentials: true,
